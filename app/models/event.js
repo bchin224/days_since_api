a50f450cf19a7ccedb3260a5da48aedf43ee1852
bchin224 <bchin224@gmail.com>
2021-01-10 02:35:55 -0500

    Built virtual -Added virtual to eventSchema

--- app/models/event.js
+++ app/models/event.js
@@ -21,4 +21,30 @@ const eventSchema = new mongoose.Schema({
   timestamps: true
 })
 
+// function to calculate elapsed days between event and today
+eventSchema.virtual('daysSince').get(function (date) {
+  let dayCounter = 0
+  let thisMonth = new Date().getMonth()
+  let thisDay = new Date().getDate()
+  let thisYear = new Date().getFullYear()
+  let pastMonth = new Date(date).getMonth()
+  let pastDay = new Date(date).getDate()
+  let pastYear = new Date(date).getFullYear()
+  if (pastMonth >= thisMonth && thisYear !== pastYear) {
+    dayCounter += ((12 - (pastMonth - thisMonth)) * 30) + 5
+  } else if (pastMonth < thisMonth) {
+    dayCounter += ((thisMonth - pastMonth) * 30) + 5
+  }
+  if (pastDay >= thisDay) {
+    dayCounter += (pastDay - thisDay)
+  } else if (pastDay < thisDay) {
+    dayCounter += (thisDay - pastDay)
+  }
+  const yearCalc = ((thisYear - pastYear) * 365) - 365
+  if (yearCalc >= 0) {
+    dayCounter += yearCalc
+  }
+  return dayCounter
+})
+
 module.exports = mongoose.model('Event', eventSchema)
